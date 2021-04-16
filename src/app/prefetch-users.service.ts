aab8712fb2a277ce2f96637ebc7e467dbe0be579
zubuxx <krownicki1@gmail.com>
2021-04-16 19:28:19 +0200

    Prefetchers implemented.

---
+++ src/app/prefetch-users.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {Resolve} from "@angular/router";
+import {Observable} from "rxjs";
+import {User} from "./model/User";
+import {DataService} from "./data.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PrefetchUsersService implements Resolve<Observable<Array<User>>>{
+
+
+  constructor(private dataService: DataService) { }
+
+  resolve() {
+    return this.dataService.getUsers();
+  }
+}
